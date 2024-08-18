8c6de82bfee7e48b419fd8e27484adb1ae5071a9
Konstantin Lushin <rule_64@mail.ru>
2024-08-19 03:20:44 +0400

    Add files via upload

---
+++ Module 1/module_1_3.py
@@ -0,0 +1,25 @@
+#name = 'Konstantin'
+#print(name)
+#age = 28
+#print(age)
+#age = (age + 2)
+#print(age)
+#is_student = True
+#print(is_student)
+
+print ('       Start programm')
+print (' ')
+name = 'Konstantin' # можно добавить еще одну переменную last name и написать в одну строку с 'name' фамилию
+print('Name: ', name)
+age = 28
+print('Age: ' , age)
+age = (age + 2) # можно было сделать через значение age = (age + age) ответ в строке бы был New Age: 56
+print('New Age: ' , age)
+is_student = True # можно выписать через условия которое даст true по типу 'print(age < 100)' ответ true
+print("Is Student: " , is_student ) #Name:  Konstantin
+                                    #Age:  28
+                                    #New Age:  30
+                                    #Is Student:  True
+print ('       The programm is ready')
+print (' ')
+print('       Thank you for the practice, have a nice day =)')
