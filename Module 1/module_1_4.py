3288c0db7dfb7ba9cd92241d6f9bebd8a6172490
Konstantin Lushin <rule_64@mail.ru>
2024-08-19 04:03:28 +0400

    Add files via upload

---
+++ Module 1/module_1_4.py
@@ -0,0 +1,12 @@
+print ('       Start programm')
+print (' ')
+my_string = input('Прошу Вас ввести вашу должность, либо указать статус "базработный": ') #  Специалист 1 категории
+print(my_string) # Специалист 1 категории
+print(my_string .upper()) # СПЕЦИАЛИСТ 1 КАТЕГОРИИ
+print(my_string .lower()) # специалист 1 категории
+print(my_string .upper().replace(' ','')) # СПЕЦИАЛИСТ1КАТЕГОРИИ
+print(my_string [0]) # С
+print(my_string [-1]) # и
+print ('       The programm is ready')
+print (' ')
+print('       Thank you for the practice, have a nice day =)')
